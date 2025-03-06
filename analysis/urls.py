21c0401e55a50455a782568f45479efdf8cfd5c2
najmulislamnajim <ninbd17@gmail.com>
2025-03-06 14:56:37 +0600

    add multiple urls

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('strong_centers',views.plot_vote_rates,name='strong_centers'),
+    path('centers/dominant/', views.center_detail, name='strongest_centers'),
+    path('centers/solid/', views.center_detail, name='strong_centers'),
+    path('centers/critical/', views.center_detail, name='critical_centers'),
+    path('centers/contested/', views.center_detail, name='competitive_centers'),
+    path('centers/struggling/', views.center_detail, name='weak_centers'),
+    path('centers/underperforming/', views.center_detail, name='very_weak_centers'),
 ]
