05149482422c84afa68945c4f06316f0bb67794d
najmulislamnajim <ninbd17@gmail.com>
2025-03-06 14:05:53 +0600

    add home view

--- analysis/views.py
+++ analysis/views.py
@@ -5,3 +5,7 @@ def plot_vote_rates(request):
     data = analyze_election_data()
     # Pass the data to the template
     return render(request, 'test.html', {'data':data})
+
+
+def home(request):
+    return render(request, 'home.html')
