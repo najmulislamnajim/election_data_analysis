58dad9a5f321060223678714e2b8e47e71e86dcb
najmulislamnajim <ninbd17@gmail.com>
2025-03-06 14:05:03 +0600

    add home url

--- election_analysis/urls.py
+++ election_analysis/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from analysis.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
     path('analysis/',include('analysis.urls')),
 ]
